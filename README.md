48062a241711362c38f577dae0b7cbc1b6f29ef2
LittleBlBb <apdragon2014@gmail.com>
2026-08-18 16:01:40 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Payment Router
+
+### Stack
+
+- Java
+- Spring Boot
+- PostgreSQL
+- Docker
+
+### Запуск PostgreSQL
+`docker compose up -d`
+
+### Запуск приложения
+`./mvnw spring-boot:run`
+
+### Проверка
+`GET http://localhost:8080/actuator/health`
---
+++ src/test/java/com/kertis/payrouter/PaymentRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kertis.payrouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentRouterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
